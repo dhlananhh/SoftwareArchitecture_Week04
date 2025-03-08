c07c766985aa87ba5008e959a3a7d2332cfe7daa
Lan Anh <dhlananh2309@gmail.com>
2025-03-08 22:15:00 +0700

    Implement Structural Design Pattern Samples; Update Proof Images; Update README file

--- StructuralDesignPatternsImplementation/src/main/java/iuh/fit/se/designs/tests/FacadePatternTest.java
+++ StructuralDesignPatternsImplementation/src/main/java/iuh/fit/se/designs/tests/FacadePatternTest.java
@@ -15,12 +15,12 @@ public class FacadePatternTest {
         MySqlHelper mySqlHelper = new MySqlHelper();
         mySqlHelper.generateMySqlHTMLReport(tableName, con);
 
-        Connection con1 = OracleHelper.getOracleDBConnection();
-        OracleHelper oracleHelper = new OracleHelper();
-        oracleHelper.generateOraclePDFReport(tableName, con1);
+//        Connection con1 = OracleHelper.getOracleDBConnection();
+//        OracleHelper oracleHelper = new OracleHelper();
+//        oracleHelper.generateOraclePDFReport(tableName, con1);
 
         //generating MySql HTML report and Oracle PDF report using Facade
         HelperFacade.generateReport(HelperFacade.DBTypes.MYSQL, HelperFacade.ReportTypes.HTML, tableName);
-        HelperFacade.generateReport(HelperFacade.DBTypes.ORACLE, HelperFacade.ReportTypes.PDF, tableName);
+        HelperFacade.generateReport(HelperFacade.DBTypes.MYSQL, HelperFacade.ReportTypes.PDF, tableName);
     }
 }
